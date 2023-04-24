d43cdcca36743dce3ec3b9908a1bb285d0d6dd54
jennystar7703 <tt7703ok@gmail.com>
2023-04-25 00:39:27 +0900

    first commit

---
+++ ClasspathExample.java
@@ -0,0 +1,6 @@
+public class ClasspathExample {
+    public static void main(String[] args) {
+      String classpath = System.getProperty("java.class.path");
+      System.out.println(classpath);
+    }
+  }
