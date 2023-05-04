cdfdd7c32b3864ddb217d0e59a866ceba3663b39
gyungsoo han <tt7703ok@gmail.com>
2023-05-04 10:51:55 +0900

    spring setting

---
+++ demo/src/test/java/info_board/demo/Info.java
@@ -0,0 +1,13 @@
+package info_board.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Info {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ info_board/src/test/java/com/jenny/info_board/InfoBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenny.info_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
